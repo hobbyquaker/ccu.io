bd35fb91d9119cdde934d0ff81d2b546fd5c363b
hobbyquaker <hobbyquaker@gmail.com>
2013-11-22 15:34:17 +0100

    geofency adapter v0.3, bugfixes, new definition of multiple devices, timetamp format

--- ccu.io/adapter/geofency/geofency.js
+++ ccu.io/adapter/geofency/geofency.js
@@ -1,16 +1,25 @@
-var express =   require('express');
+/**
+ *      CCU.IO Geofency Adapter
+ *      11'2013 Hobbyquaker
+ *
+ *      Version 0.3
+ *
+ *
+ */
+
+
 var settings = require(__dirname+'/../../settings.js');
 
 if (!settings.adapters.geofency || !settings.adapters.geofency.enabled) {
     process.exit();
 }
 
-var geoSettings = settings.adapters.geofency.settings;
-
-
-var logger =    require(__dirname+'/../../logger.js'),
-    io =        require('socket.io-client');
-
+var geoSettings = settings.adapters.geofency.settings,
+    firstId = settings.adapters.geofency.firstId,
+    express =   require('express'),
+    logger =    require(__dirname+'/../../logger.js'),
+    io =        require('socket.io-client'),
+    app = express();
 
 if (settings.ioListenPort) {
     var socket = io.connect("127.0.0.1", {
@@ -19,12 +28,11 @@ if (settings.ioListenPort) {
 } else if (settings.ioListenPortSsl) {
     var socket = io.connect("127.0.0.1", {
         port: settings.ioListenPortSsl,
-        secure: true,
+        secure: true
     });
 } else {
     process.exit();
 }
-
 
 socket.on('connect', function () {
     logger.info("adapter geof. connected to ccu.io");
@@ -34,17 +42,11 @@ socket.on('disconnect', function () {
     logger.info("adapter geof. disconnected from ccu.io");
 });
 
-socket.on('event', function (obj) {
-    if (!obj || !obj[0]) {
-        return;
-    }
-});
-
 function stop() {
     logger.info("adapter geof. terminating");
     setTimeout(function () {
         process.exit();
-    }, 250);
+    }, 500);
 }
 
 process.on('SIGINT', function () {
@@ -55,59 +57,65 @@ process.on('SIGTERM', function () {
     stop();
 });
 
-var i = 0;
-for (var id in geoSettings.devices) {
-    socket.emit("setObject", geoSettings.devices[id], {
-        Name: "Geofency Name "+i,
-        DPInfo: id,
-        TypeName: "VARDP"
-    });
-    socket.emit("setObject", geoSettings.devices[id]+1, {
-        Name: "Geofency Longitude "+i,
-        DPInfo: id,
-        TypeName: "VARDP"
-    });
-    socket.emit("setObject", geoSettings.devices[id]+2, {
-        Name: "Geofency Latitude "+i,
-        DPInfo: id,
-        TypeName: "VARDP"
-    });
-    socket.emit("setObject", geoSettings.devices[id]+3, {
-        Name: "Geofency Entry "+i,
-        DPInfo: id,
-        TypeName: "VARDP"
-    });
-    socket.emit("setObject", geoSettings.devices[id]+4, {
-        Name: "Geofency Date "+i,
-        DPInfo: id,
-        TypeName: "VARDP"
-    });
-    i += 1;
-}
-
-
-app =  express();
 
 app.use(express.basicAuth(geoSettings.user, geoSettings.pass));
 
-var server =    require('http').createServer(app);
-
+var server = require('http').createServer(app);
 server.listen(geoSettings.port);
 
+for (var i = 0; i < geoSettings.devices.length; i++) {
+    var dpId = firstId + (i * 5);
+
+    socket.emit("setObject", dpId, {
+        Name: "Geofency Name "+geoSettings.devices[i],
+        TypeName: "VARDP"
+    });
+    socket.emit("setObject", dpId+1, {
+        Name: "Geofency Longitude "+geoSettings.devices[i],
+        TypeName: "VARDP"
+    });
+    socket.emit("setObject", dpId+2, {
+        Name: "Geofency Latitude "+geoSettings.devices[i],
+        TypeName: "VARDP"
+    });
+    socket.emit("setObject", dpId+3, {
+        Name: "Geofency Entry "+geoSettings.devices[i],
+        TypeName: "VARDP"
+    });
+    socket.emit("setObject", dpId+4, {
+        Name: "Geofency Date "+geoSettings.devices[i],
+        TypeName: "VARDP"
+    });
+}
+
 app.use(express.bodyParser({}));
-app.post('/', function (req, res) {
-    if (geoSettings.devices[req.body.id]) {
-        logger.info("adapter geof. received webhook from id "+req.body.id);
-        socket.emit("setState", [geoSettings.devices[req.body.id], req.body.name]);
-        socket.emit("setState", [geoSettings.devices[req.body.id]+1, req.body.longitude]);
-        socket.emit("setState", [geoSettings.devices[req.body.id]+2, req.body.latitude]);
-        socket.emit("setState", [geoSettings.devices[req.body.id]+3, req.body.entry]);
-        socket.emit("setState", [geoSettings.devices[req.body.id]+4, req.body.date]);
-
-    } else {
-        logger.warn("adapter geof. received webhook from unknown id "+req.body.id);
-    }
-
+app.post('/*', function (req, res) {
     res.set('Content-Type', 'text/html');
-    res.send("<!DOCTYPE html><html><body>OK</body></html>");
+    var id = parseInt(req.path.slice(1), 10);
+    if (geoSettings.devices[id]) {
+        var dpId = firstId + (id * 5);
+        logger.info("adapter geof. received webhook from device "+id);
+        socket.emit("setState", [dpId, req.body.name]);
+        socket.emit("setState", [dpId+1, req.body.longitude]);
+        socket.emit("setState", [dpId+2, req.body.latitude]);
+        socket.emit("setState", [dpId+3, req.body.entry]);
+        socket.emit("setState", [dpId+4, formatTimestamp(req.body.date)]);
+        res.status(200);
+        res.send("<!DOCTYPE html><html><body><h1>OK</h1></body></html>");
+    } else {
+        logger.warn("adapter geof. received webhook for unknown device "+id);
+        res.status(404);
+        res.send("<!DOCTYPE html><html><body><h1>404</h1></body></html>");
+    }
 });
+
+function formatTimestamp(str) {
+    var timestamp = new Date(str);
+    var ts = timestamp.getFullYear() + '-' +
+        ("0" + (timestamp.getMonth() + 1).toString(10)).slice(-2) + '-' +
+        ("0" + (timestamp.getDate()).toString(10)).slice(-2) + ' ' +
+        ("0" + (timestamp.getHours()).toString(10)).slice(-2) + ':' +
+        ("0" + (timestamp.getMinutes()).toString(10)).slice(-2) + ':' +
+        ("0" + (timestamp.getSeconds()).toString(10)).slice(-2);
+    return ts;
+}
