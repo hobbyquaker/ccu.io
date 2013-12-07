af5e068ac7fd7bd0c1c5f1fa28670f4146da36a4
GermanBluefox <dogafox@gmail.com>
2013-12-07 20:10:15 +0100

    - Add some missing files

---
+++ scripts/global.js
@@ -0,0 +1,26 @@
+/**
+ *
+ *  _global.js
+ *
+ *  Hier können Funktionen definiert werden die in allen Scripten zur Verfügung stehen
+ *
+ *  Hier sollten auch evtl benötigte Nodejs Module eingebunden werden
+ *
+ */
+
+function commaSeparatedList(arr) {
+    var text = "";
+    var length = arr.length;
+    if (length > 0) {
+        var first = true;
+        for (var i=0; i<length; i++) {
+            if (!first) {
+                text = text + ", ";
+            } else {
+                first = false;
+            }
+            text = text + arr[i];
+        }
+    }
+    return text;
+}
