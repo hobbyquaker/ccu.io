b608fcb262791c1998037cff1e4f5bf0e08a2dd4
hobbyquaker <hobbyquaker@gmail.com>
2013-12-15 09:41:02 +0100

    rpi-adapter supports GPIOs

--- adapter/rpi/rpi.js
+++ adapter/rpi/rpi.js
@@ -1,27 +1,31 @@
 var settings = require(__dirname+'/../../settings.js');
 
+
 if (!settings.adapters.rpi || !settings.adapters.rpi.enabled) {
     process.exit();
 }
 
+var adapterSettings = settings.adapters.rpi.settings;
+
 var logger =    require(__dirname+'/../../logger.js'),
     io =        require('socket.io-client'),
     fs =        require("fs");
-
+var gpio = require("gpio");
+var gpioIDs = {};
+var gpioObjs = {};
 
 if (settings.ioListenPort) {
-    var socket = io.connect("127.0.0.1", {
+    var socket = io.connect(settings.binrpc.listenIp, {
         port: settings.ioListenPort
     });
 } else if (settings.ioListenPortSsl) {
-    var socket = io.connect("127.0.0.1", {
+    var socket = io.connect(settings.binrpc.listenIp, {
         port: settings.ioListenPortSsl,
-        secure: true,
+        secure: true
     });
 } else {
     process.exit();
 }
-
 
 socket.on('connect', function () {
     logger.info("adapter rpi   connected to ccu.io");
@@ -47,7 +51,24 @@ process.on('SIGTERM', function () {
 });
 
 
-socket.emit("setObject", 72500, {
+socket.on('event', function (obj) {
+    if (!obj || !obj[0]) {
+        return;
+    }
+    var id = obj[0];
+    var val = obj[1];
+
+    if (gpioObjs[id]) {
+        if (val && val !== "0") {
+            gpioObjs[id].set();
+        } else {
+            gpioObjs[id].reset();
+        }
+    }
+
+});
+
+socket.emit("setObject", settings.adapters.rpi.firstId, {
     Name: "RPI",
     TypeName: "DEVICE",
     HssType: "RPI",
@@ -58,44 +79,95 @@ socket.emit("setObject", 72500, {
     ]
 });
 
-socket.emit("setObject", 72501, {
+socket.emit("setObject", settings.adapters.rpi.firstId+1, {
     Name: "RPI.SENSORS",
     TypeName: "CHANNEL",
     Address: "RPI.SENSORS",
     HssType: "RPI_SENSORS",
     DPs: {
-        TEMPERATURE: 72502
+        TEMPERATURE: settings.adapters.rpi.firstId+2
     },
-    Parent: 72500
+    Parent: settings.adapters.rpi.firstId
 });
 
-socket.emit("setObject", 72503, {
+socket.emit("setObject", settings.adapters.rpi.firstId+3, {
     Name: "RPI.SYSTEM",
     TypeName: "CHANNEL",
     Address: "RPI.SYSTEM",
     HssType: "RPI_SYSTEM",
     DPs: {
-        LOAD: 72504
+        LOAD: settings.adapters.rpi.firstId+4
     },
-    Parent: 72500
+    Parent: settings.adapters.rpi.firstId
 });
 
-socket.emit("setObject", 72502, {
+
+var dpId = settings.adapters.rpi.firstId+5;
+for (var gpioNr in adapterSettings.gpio) {
+    socket.emit("setObject", dpId, {
+        Name: "RPI.GPIO"+gpioNr,
+        TypeName: "CHANNEL",
+        Address: "RPI.GPIO"+gpioNr,
+        HssType: "RPI_GPIO",
+        DPs: {
+            DIRECTION: dpId+1,
+            STATE: dpId+2
+        },
+        Parent: settings.adapters.rpi.firstId
+    });
+    socket.emit("setObject", dpId+1, {
+        "Name": "RPI.GPIO"+gpioNr+".DIRECTION",
+        "TypeName": "HSSDP",
+        "Parent": dpId,
+
+    });
+
+    socket.emit("setObject", dpId+2, {
+        "Name": "RPI.GPIO"+gpioNr+".STATE",
+        "TypeName": "HSSDP",
+        "Parent": dpId
+    });
+    gpioIDs[gpioNr] = dpId+2;
+
+
+    gpioObjs[dpId+2] = gpio.export(parseInt(gpioNr,10), {
+        direction: adapterSettings.gpio[gpioNr].direction,
+        ready: function() {
+
+
+        }
+    });
+
+    if (adapterSettings.gpio[gpioNr].direction == "in") {
+        var dpIn = dpId+2
+        gpioObjs[dpId+2].on("change", function(val) {
+            socket.emit("setState", [dpIn, val]);
+        });
+    }
+
+    socket.emit("setState", [dpId+1, adapterSettings.gpio[gpioNr].direction]);
+
+    dpId += 3;
+
+
+}
+
+socket.emit("setObject", settings.adapters.rpi.firstId+2, {
     "Name": "RPI.SENSORS.TEMPERATURE",
     "TypeName": "HSSDP",
     "Operations": 5,
     "ValueType": 4,
     "ValueUnit": "°C",
-    "Parent": 72501
+    "Parent": settings.adapters.rpi.firstId+1
 });
 
-socket.emit("setObject", 72504, {
+socket.emit("setObject", settings.adapters.rpi.firstId+4, {
     "Name": "RPI.SYSTEM.LOAD",
     "TypeName": "HSSDP",
     "Operations": 5,
     "ValueType": 4,
     "ValueUnit": "",
-    "Parent": 72503
+    "Parent": settings.adapters.rpi.firstId+3
 });
 
 function getValues() {
@@ -103,8 +175,8 @@ function getValues() {
     var loadavg = fs.readFileSync("/proc/loadavg").toString().split(" ");
     temp = parseFloat(temp) / 1000;
     temp = temp.toFixed(1);
-    socket.emit("setState", [72502, temp]);
-    socket.emit("setState", [72504, parseFloat(loadavg[0])]);
+    socket.emit("setState", [settings.adapters.rpi.firstId+2, temp]);
+    socket.emit("setState", [settings.adapters.rpi.firstId+4, parseFloat(loadavg[0])]);
 }
 
 getValues();
