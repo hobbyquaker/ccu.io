0e470df5f167baa7c228771a0a63805c81423b06
hobbyquaker <hobbyquaker@gmail.com>
2013-12-26 22:11:18 +0100

    Bugfix getState

--- script-engine.js
+++ script-engine.js
@@ -758,7 +758,7 @@ function setState(id, val, callback) {
 
 function getState(id, dpType) {
     var dp = datapoints[findDatapoint(id, dpType)];
-    if (dp[0]) {
+    if (dp && dp[0]) {
         return dp[0];
     } else {
         return undefined;
@@ -849,7 +849,7 @@ function findDatapoint(needle, hssdp) {
                     needle = regaObjects[needle].DPs[hssdp];
                 }
             }
-        } else if (needle.match(/[a-zA-Z-]+\.[0-9A-Za-z-]+:[0-9]+\.[A-Z_]+/)) {
+        } else if (needle.toString().match(/[a-zA-Z-]+\.[0-9A-Za-z-]+:[0-9]+\.[A-Z_]+/)) {
             // Get by full BidCos-Address
             addrArr = needle.split(".");
             if (regaIndex.Address[addrArr[1]]) {
