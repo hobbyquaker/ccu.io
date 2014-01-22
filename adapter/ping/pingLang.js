605a6cca68d72bff4b0f807af3dff7d37bf47ac0
GermanBluefox <deniskhaev@gmail.com>
2014-01-22 15:21:28 +0100

    Create pingLang.js

---
+++ adapter/ping/pingLang.js
@@ -0,0 +1,11 @@
+var adapterWords = {
+  "false":             {"en": "false",             "de": "nein",            "ru": "нет"},
+  "true":              {"en": "true",              "de": "ja",              "ru": "да"},
+  "Enabled:":          {"en": "Enabled:",          "de": "Aktiviert:",      "ru": "Активно:"},
+  "Polling interval:": {"en": "Polling interval (sec):", "de": "Pollintervall (sek):",     "ru": "Интервал сканирования (сек):"},
+  "IP address":        {"en": "IP address",        "de": "IP-Adresse",      "ru": "IP адрес"},
+  "Name":              {"en": "Name",              "de": "Name",            "ru": "Имя"},
+  "Rooms":             {"en": "Rooms",             "de": "Zimmern",         "ru": "Комнаты"},
+  "Functions":         {"en": "Functions",         "de": "Gewerke",         "ru": "Раздел"},
+  "Favorites":         {"en": "Favorites",         "de": "Favorites",       "ru": "Избранное"}
+};
