874d0bcb657c977c0da88d281971f93c444250e5
hobbyquaker <hobbyquaker@gmail.com>
2014-01-24 13:15:46 +0100

    manual merge

--- adapter/dream/dream.js
+++ adapter/dream/dream.js
@@ -3,21 +3,15 @@
  *      01'2014 BasGo
  *      mail: basgo@gmx.de 
  *
- *      Version 0.1
+ *      Version 0.8
  *
- *      Possible commands:
- *      MESSAGE:Messagetext
- *      REBOOT
- *      RESTART
- *      STANDBY
- *      WAKEUP
- *      MUTE
- *      VOLUME:1-100
+ *      development at https://github.com/BasGo/ccu.io/tree/master/adapter/dream
+ *
  */
 var settings = require(__dirname + '/../../settings.js');
 
 if (!settings.adapters.dream || !settings.adapters.dream.enabled) {
-    process.exit();
+	process.exit();
 }
 
 var dreamSettings = settings.adapters.dream.settings;
@@ -30,8 +24,6 @@ var logger		= require(__dirname + '/../../logger.js'),
 	http		= require('http'),
 	querystring	= require('querystring'),
 	xml2js		= require("xml2js");
-
-var boxUrl = "http://"+dreamSettings.ip+"/web";
 
 var objects = {},
 	datapoints = {};
@@ -49,36 +41,44 @@ if (settings.ioListenPort) {
 	process.exit();
 }
 
+function logDebug(message) {
+	if (dreamSettings.debugEnabled) {
+		logger.info("adapter dream " + message);
+	}
+}
+
+function logInfo(message) {
+	logger.info("adapter dream " + message);
+}
+
+function logWarning(message) {
+	logger.warn("adapter dream " + message);
+}
+
 socket.on('connect', function () {
-	logger.info("adapter dream connected to ccu.io");
+	logInfo("connected to ccu.io");
 });
 
 socket.on('disconnect', function () {
-	logger.info("adapter dream disconnected from ccu.io");
+	logInfo("disconnected from ccu.io");
 });
-
-function debugLog(message) {
-	if (dreamSettings.debugEnabled) {
-		logger.info(message);
-	}
-}
 
 socket.on('event', function (obj) {
 	if (!obj || !obj[0]) {
 		return;
 	}
-	
+
 	var dpId = obj[0];			// ID des geänderten Objektes
 	var dpVal = obj[1];			// Wert des geänderten Objektes
 	var dpTs = obj[2];			// Timestamp der letzten Änderung
 	var dpAck = obj[3];			// ACKnowledge der letzten Änderung
-	
+
 	// get command datapoints for single boxes, because they need to be watched for changes
 	for (var id_ in dreamSettings.boxes) {
 		var id = parseInt(id_.substring(1));
 		var boxId = (dreamSettings.firstId) + (id * 10);
 		if (dpId == boxId && dpVal != "") {
-			debugLog("adapter dream event: "+dpId+" "+dpVal+" "+dpTs+" "+dpAck+" "+obj);
+			logDebug("received event: "+dpId+" "+dpVal+" "+dpTs+" "+dpAck+" "+obj);
 			var valString = dpVal.toString();
 			var args = valString.split(":");
 			if (args.length == 2) {
@@ -86,16 +86,14 @@ socket.on('event', function (obj) {
 			} else if (args.length == 1) {
 				executeCommand(args[0], "", id_);
 			} else {
-				logger.warn("adapter dream expects a command having syntax COMMAND:VALUE");
+				logWarning("expects a command having syntax COMMAND:VALUE");
 			}
 		}
 	}
-	
-
 });
 
 function stop() {
-	logger.info("adapter dream terminating");
+	logInfo("terminating");
 	setTimeout(function () {
 		process.exit();
 	}, 250);
@@ -110,87 +108,88 @@ process.on('SIGTERM', function () {
 });
 
 function setObject(id, obj) {
-    objects[id] = obj;
-    datapoints[id] = [obj.Value];
-    socket.emit("setObject", id, obj);
+	objects[id] = obj;
+	datapoints[id] = [obj.Value];
+	logDebug("sets object with ID "+id+" and name "+obj.Name+" to value '"+obj.Value+"'");
+	socket.emit("setObject", id, obj);
 }
 
 function setState(id, val) {
 	datapoints[id] = [val];
-	debugLog("DREAM - setState for ID "+id+" to value '"+val+"'");
+	logDebug("sets state for ID "+id+" to value '"+val+"'");
 	socket.emit("setState", [id,val,null,true]);
 }
 
 function DreamInit() {
 	var i = 0;
-	
+
 	for (var id_ in dreamSettings.boxes) {
+		logDebug("initializes box with deviceId "+id_);
 		var id = parseInt (id_.substring(1));
 		var boxId = (dreamSettings.firstId) + (id * 10);
-		var boxName = (dreamSettings.boxes[id_]['name']) ? dreamSettings.boxes[id_]['name'] : dreamSettings.boxes[id_].ip;
+		var boxName = (dreamSettings.boxes[id_]['name']) ? dreamSettings.boxes[id_]['name'] : dreamSettings.boxes[id_].ip.replace(/\./g,"_");
 		var boxNameUpper = boxName.toUpperCase();
 		setObject(boxId, {
-				Name: "DREAM."+boxNameUpper+".COMMAND",
-				TypeName: "VARDP",
-				Value : ""
-			});
-			
-			setObject(boxId + 1, {
-				Name: "DREAM."+boxNameUpper+".STANDBY",
-				TypeName: "VARDP",
-				Value : false
-			});
-			
-			setObject(boxId + 2, {
-				Name: "DREAM."+boxNameUpper+".VOLUME",
-				TypeName: "VARDP",
-				Value : "0"
-			});
-			
-			setObject(boxId + 3, {
-				Name: "DREAM."+boxNameUpper+".MUTED",
-				TypeName: "VARDP",
-				Value : false
-			});
-			
-			setObject(boxId + 4, {
-				Name: "DREAM."+boxNameUpper+".CHANNEL",
-				TypeName: "VARDP",
-				Value : ""
-			});
-			
-			setObject(boxId + 5, {
-				Name: "DREAM."+boxNameUpper+".HDD.CAPACITY",
-				TypeName: "VARDP",
-				Value : ""
-			});
-			
-			setObject(boxId + 6, {
-				Name: "DREAM."+boxNameUpper+".HDD.FREE",
-				TypeName: "VARDP",
-				Value : ""
-			});
+			Name: "DREAM."+boxNameUpper+".COMMAND",
+			TypeName: "VARDP",
+			Value : ""
+		});
+
+		setObject(boxId + 1, {
+			Name: "DREAM."+boxNameUpper+".STANDBY",
+			TypeName: "VARDP",
+			Value : false
+		});
+
+		setObject(boxId + 2, {
+			Name: "DREAM."+boxNameUpper+".VOLUME",
+			TypeName: "VARDP",
+			Value : "0"
+		});
+
+		setObject(boxId + 3, {
+			Name: "DREAM."+boxNameUpper+".MUTED",
+			TypeName: "VARDP",
+			Value : false
+		});
+
+		setObject(boxId + 4, {
+			Name: "DREAM."+boxNameUpper+".CHANNEL",
+			TypeName: "VARDP",
+			Value : ""
+		});
+
+		setObject(boxId + 5, {
+			Name: "DREAM."+boxNameUpper+".HDD.CAPACITY",
+			TypeName: "VARDP",
+			Value : ""
+		});
+
+		setObject(boxId + 6, {
+			Name: "DREAM."+boxNameUpper+".HDD.FREE",
+			TypeName: "VARDP",
+			Value : ""
+		});
 	}
 	
-	logger.info("adapter dream objects inserted, starting at: "+dreamSettings.firstId);
+	logDebug("objects inserted, starting at: "+dreamSettings.firstId);
 	
 	if (dreamSettings.pollingEnabled) {
 		// Fix polling interval if too short
 		if (dreamSettings.pollingInterval <= 5000 * (i + 1)) {
 			dreamSettings.pollingInterval = 5000 * (i + 1);
 		}
-		
-		logger.info("adapter dream polling enabled - interval " + dreamSettings.pollingInterval + " ms");
-		
+
+		logInfo("polling enabled - interval " + dreamSettings.pollingInterval + " ms");
+
 		setInterval(checkStatus, dreamSettings.pollingInterval);
 		checkStatus();
 	} else {
-		logger.info("adapter dream polling has been disabled by configuration");
+		logInfo("polling has been disabled by configuration");
 	}
 }
 
 function getResponse (command, deviceId, path, callback){
-	debugLog("getResponse - command: "+command+", deviceId: "+deviceId+", path: "+path);
 	var device = dreamSettings.boxes[deviceId];
 	var options = {
 		host: device.ip,
@@ -198,8 +197,21 @@ function getResponse (command, deviceId, path, callback){
 		path: path,
 		method: 'GET'
 	};
+	
+	logDebug("creating request for command '"+command+"' (deviceId: "+deviceId+", host: "+options.host+", port: "+options.port+", path: '"+options.path+"')");
+	
+	if (typeof device.username != 'undefined' && typeof device.password != 'undefined') {
+		if (device.username.length > 0 && device.password.length > 0) {
+			options.headers = {
+				'Authorization': 'Basic ' + new Buffer(device.username + ':' + device.password).toString('base64')
+			}
+			logDebug("using authorization with user '"+device.username+"'");
+		} else {
+			logDebug("using no authorization");
+		}
+	}
 
-	debugLog("getResponse - host: "+options.host+", port: "+options.port);
+	
 	var req = http.get(options, function(res) {
 		var pageData = "";
 		res.setEncoding('utf8');
@@ -207,7 +219,6 @@ function getResponse (command, deviceId, path, callback){
 			pageData += chunk;
 		});
 		res.on('end', function () {
-	  		//logger.info("Trying to parse: "+pageData);
 			var parser = new xml2js.Parser();
 			parser.parseString(pageData, function (err, result) {
 				if (callback) {
@@ -217,13 +228,13 @@ function getResponse (command, deviceId, path, callback){
 		});
 	});
 	req.on('error', function(e) {
-		logger.warn("adapter dream received error: "+e.message);
+		logWarning("received error: "+e.message);
 	});
 
 }
 
 function parseBool(string){
-	var cleanedString = string[0].replace(/(\r\n|\n|\r)/gm,"");
+	var cleanedString = string[0].replace(/(\t\n|\n|\t)/gm,"");
 	switch(cleanedString.toLowerCase()){
 		case "true": case "yes": case "1": return true;
 		default: return false;
@@ -231,7 +242,7 @@ function parseBool(string){
 }
 
 function evaluateCommandResponse (command, deviceId, xml) {
-	debugLog("evaluateCommandResponse - command: "+command+", received XML: "+JSON.stringify(xml));
+	logDebug("evaluating response for command '"+command+"': "+JSON.stringify(xml));
 	
 	var id = parseInt(deviceId.substring(1));
 	var boxId = (dreamSettings.firstId) + (id * 10);
@@ -246,7 +257,7 @@ function evaluateCommandResponse (command, deviceId, xml) {
 			break;
 		case "MUTE":
 		case "UNMUTE":
-		case "TOOGLEMUTE":
+		case "MUTE_TOGGLE":
 		case "VOLUME":
 			setState(boxId + 2, parseInt(xml.e2volume.e2current[0]));	// 20
 			setState(boxId + 3, parseBool(xml.e2volume.e2ismuted));		// True|False
@@ -254,6 +265,7 @@ function evaluateCommandResponse (command, deviceId, xml) {
 			break;
 		case "WAKEUP":
 		case "STANDBY":
+		case "OFF":
 		case "TOOGLESTANDBY":
 			setState(boxId + 1, parseBool(xml.e2powerstate.e2instandby));		// true|false
 			setState(boxId, "");
@@ -270,13 +282,30 @@ function evaluateCommandResponse (command, deviceId, xml) {
 			setState(boxId + 5, xml.e2abouts.e2about[0].e2hddinfo[0].capacity[0]);		// 500.107 GB
 			setState(boxId + 6, xml.e2abouts.e2about[0].e2hddinfo[0].free[0]);			// 100.273 GB
 			break;
+		case "KEY":
+		case "VOLUME_UP":
+		case "VOLUME_DOWN":
+		case "LEFT":
+		case "RIGHT":
+		case "UP":
+		case "DOWN":
+		case "EXIT":
+		case "CH_UP":
+		case "CH_DOWN":
+		case "SELECT":
+		case "OK":
+		case "BOUQUET_UP":
+		case "BOUQUET_DOWN":
+		case "INFO":
+		case "MENU":
+			setState(boxId, "");
 		default:
-			logger.warn("adapter dream received unknown command '"+command+"' @ evaluateCommandResponse");
+			logWarning("received unknown command '"+command+"' @ evaluateCommandResponse");
 	}
 }
 
 function executeCommand(command, value, deviceId) {
-	logger.info("executeCommand - command: "+command+", value: "+value+", deviceId: "+deviceId);
+	logDebug("executing command '"+command+"' with value '"+value+"' for device '"+deviceId+"'");
 	var id = parseInt(deviceId.substring(1));
 	var boxId = (dreamSettings.firstId) + (id * 10);
 	
@@ -286,6 +315,48 @@ function executeCommand(command, value, deviceId) {
 			var msgTimeout = parseInt(dreamSettings.messageTimeout);
 			var msgType = parseInt(dreamSettings.messageType);
 			getResponse (command, deviceId, "/web/message?text="+querystring.escape(value)+"&type="+msgType+"&timeout="+msgTimeout, evaluateCommandResponse);
+			break;
+		case "KEY":
+			getResponse (command, deviceId, "/web/remotecontrol?command="+querystring.escape(value), evaluateCommandResponse);
+			break;
+		case "VOLUME_UP":
+			getResponse (command, deviceId, "/web/remotecontrol?command=115", evaluateCommandResponse);
+			break;
+		case "VOLUME_DOWN":
+			getResponse (command, deviceId, "/web/remotecontrol?command=114", evaluateCommandResponse);
+			break;
+		case "BOUQUET_UP":
+			getResponse (command, deviceId, "/web/remotecontrol?command=402", evaluateCommandResponse);
+			break;
+		case "BOUQUET_DOWN":
+			getResponse (command, deviceId, "/web/remotecontrol?command=403", evaluateCommandResponse);
+			break;
+		case "LEFT":
+		case "CH_DOWN":
+			getResponse (command, deviceId, "/web/remotecontrol?command=105", evaluateCommandResponse);
+			break;
+		case "RIGHT":
+		case "CH_UP":
+			getResponse (command, deviceId, "/web/remotecontrol?command=106", evaluateCommandResponse);
+			break;
+		case "UP":
+			getResponse (command, deviceId, "/web/remotecontrol?command=103", evaluateCommandResponse);
+			break;
+		case "DOWN":
+			getResponse (command, deviceId, "/web/remotecontrol?command=108", evaluateCommandResponse);
+			break;
+		case "EXIT":
+			getResponse (command, deviceId, "/web/remotecontrol?command=174", evaluateCommandResponse);
+			break;
+		case "INFO":
+			getResponse (command, deviceId, "/web/remotecontrol?command=358", evaluateCommandResponse);
+			break;
+		case "MENU":
+			getResponse (command, deviceId, "/web/remotecontrol?command=139", evaluateCommandResponse);
+			break;
+		case "SELECT":
+		case "OK":
+			getResponse (command, deviceId, "/web/remotecontrol?command=352", evaluateCommandResponse);
 			break;
 		case "MUTE":
 			if (datapoints[boxId + 3] == "false") {
@@ -301,7 +372,7 @@ function executeCommand(command, value, deviceId) {
 				setState(boxId, "");
 			}
 			break;
-		case "TOOGLEMUTE":
+		case "MUTE_TOGGLE":
 			getResponse (command, deviceId, "/web/vol?set=mute", evaluateCommandResponse);
 			break;
 		case "VOLUME":
@@ -310,7 +381,7 @@ function executeCommand(command, value, deviceId) {
 				getResponse (command, deviceId, "/web/vol?set=set" + volume, evaluateCommandResponse);
 			}
 			break;
-		case "TOOGLESTANDBY":
+		case "STANDBY_TOGGLE":
 			getResponse (command, deviceId, "/web/powerstate?newstate=0", evaluateCommandResponse);
 			break;
 		case "DEEPSTANDBY":
@@ -326,19 +397,16 @@ function executeCommand(command, value, deviceId) {
 			getResponse (command, deviceId, "/web/powerstate?newstate=4", evaluateCommandResponse);
 			break;
 		case "STANDBY":
+		case "OFF":
 			getResponse (command, deviceId, "/web/powerstate?newstate=5", evaluateCommandResponse);
 			break;
 		default:
-			logger.warn("adapter dream received unknown command '"+command+"' @ executeCommand");
+			logWarning("received unknown command '"+command+"' @ executeCommand");
 	}
 }
 
 function checkStatus() {
 	for (var id_ in dreamSettings.boxes) {
-		var id = parseInt(id_.substring(1));
-		var boxId = (dreamSettings.firstId) + (id * 10);
-		var boxName = "DREAM."+(dreamSettings.boxes[id_]['name']) ? dreamSettings.boxes[id_]['name'] : dreamSettings.boxes[id_].ip;
-
 		ping.sys.probe(dreamSettings.boxes[id_]['ip'], function (isAlive) {
 			if (isAlive) {
 				getResponse ("GETSTANDBY", id_, "/web/powerstate", evaluateCommandResponse);
