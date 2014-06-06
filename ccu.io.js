b0a3481e267047e4cfe9dbebdffcfb868a1cf64b
hobbyquaker <hobbyquaker@gmail.com>
2014-06-06 22:47:53 +0200

    updated version and readme

--- ccu.io.js
+++ ccu.io.js
@@ -13,7 +13,7 @@
 
 var settings = require(__dirname+'/settings.js');
 
-settings.version = "1.0.36";
+settings.version = "1.0.37";
 settings.basedir = __dirname;
 settings.datastorePath = __dirname+"/datastore/";
 settings.stringTableLanguage = settings.stringTableLanguage || "de";
