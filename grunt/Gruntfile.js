5916e983bfa20a57d862721d2ac03609a9e0e6b7
GermanBluefox <dogafox@gmail.com>
2014-07-13 17:25:04 +0200

    (Bluefox) Fix grunt for debian

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function(grunt) {
         lineending: {               // Task
             dist: {                   // Target
                 options: {              // Target options
-                    eol: 'cr',
+                    eol: 'lf',
                     overwrite: true
                 },
                 files: [
--- grunt/debian-pi/etc/init.d/ccu.io.sh
+++ grunt/debian-pi/etc/init.d/ccu.io.sh
@@ -1,1 +1,45 @@
-#!/bin/bash### BEGIN INIT INFO# Provides:          ccu.io.sh# Required-Start:    $network $local_fs $remote_fs# Required-Stop::    $network $local_fs $remote_fs# Default-Start:     2 3 4 5# Default-Stop:      0 1 6# Short-Description: starts ccu.io# Description:       starts ccu.io### END INIT INFO(( EUID )) && echo .You need to have root privileges.. && exit 1PIDF=/opt/ccu.io/ccu.io.pidNODECMD=/usr/local/bin/nodeIOBROKERCMD=/opt/ccu.io/ccu.io-server.jsRETVAL=0IOBROKERUSER=@@userstart() {            echo -n "Starting ccu.io"            sudo -u ${IOBROKERUSER} $NODECMD $IOBROKERCMD start            RETVAL=$?}stop() {            echo -n "Stopping ccu.io"            sudo -u ${IOBROKERUSER} $NODECMD $IOBROKERCMD stop            RETVAL=$?}case "$1" in    start)      start  ;;    stop)      stop  ;;    restart)      stop      start  ;;    *)      echo "Usage: ccu.io {start|stop|restart}"      exit 1  ;;esacexit $RETVAL
+#!/bin/bash
+### BEGIN INIT INFO
+# Provides:          ccu.io.sh
+# Required-Start:    $network $local_fs $remote_fs
+# Required-Stop::    $network $local_fs $remote_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: starts ccu.io
+# Description:       starts ccu.io
+### END INIT INFO
+(( EUID )) && echo .You need to have root privileges.. && exit 1
+PIDF=/opt/ccu.io/ccu.io.pid
+NODECMD=/usr/local/bin/node
+IOBROKERCMD=/opt/ccu.io/ccu.io-server.js
+RETVAL=0
+IOBROKERUSER=@@user
+
+start() {
+            echo -n "Starting ccu.io"
+            sudo -u ${IOBROKERUSER} $NODECMD $IOBROKERCMD start
+            RETVAL=$?
+}
+
+stop() {
+            echo -n "Stopping ccu.io"
+            sudo -u ${IOBROKERUSER} $NODECMD $IOBROKERCMD stop
+            RETVAL=$?
+}
+case "$1" in
+    start)
+      start
+  ;;
+    stop)
+      stop
+  ;;
+    restart)
+      stop
+      start
+  ;;
+    *)
+      echo "Usage: ccu.io {start|stop|restart}"
+      exit 1
+  ;;
+esac
+exit $RETVAL
